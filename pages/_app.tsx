3e60613c1224eb6e48e367c6428aec846eaa2ab6
FeeinCof <tuan.letrong.dev@gmail.com>
2023-02-08 18:56:15 +0700

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { config } from "@fortawesome/fontawesome-svg-core";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,5 +16,10 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
